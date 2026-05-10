2dce93932f00f648b9a498d0cb1966fb41952112
Rilzkun <ranggalawe58191@gmail.com>
2026-05-10 20:49:40 +0800

    tes

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -37,14 +37,21 @@ Partial Class Form1
         Button4 = New Button()
         Panel6 = New Panel()
         Button5 = New Button()
+        Panel7 = New Panel()
+        Button6 = New Button()
+        Panel8 = New Panel()
+        Button7 = New Button()
+        Panel9 = New Panel()
+        Button8 = New Button()
         Panel1.SuspendLayout()
         CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
         FlowLayoutPanel1.SuspendLayout()
         Panel2.SuspendLayout()
         Panel3.SuspendLayout()
-        Panel4.SuspendLayout()
-        Panel5.SuspendLayout()
         Panel6.SuspendLayout()
+        Panel7.SuspendLayout()
+        Panel8.SuspendLayout()
+        Panel9.SuspendLayout()
         SuspendLayout()
         ' 
         ' Panel1
@@ -81,7 +88,7 @@ Partial Class Form1
         ' 
         FlowLayoutPanel1.BackColor = Color.Black
         FlowLayoutPanel1.Controls.Add(Panel2)
-        FlowLayoutPanel1.Location = New Point(0, 35)
+        FlowLayoutPanel1.Location = New Point(1, 35)
         FlowLayoutPanel1.Name = "FlowLayoutPanel1"
         FlowLayoutPanel1.Size = New Size(166, 420)
         FlowLayoutPanel1.TabIndex = 1
@@ -92,18 +99,20 @@ Partial Class Form1
         Panel2.Controls.Add(Button1)
         Panel2.Location = New Point(3, 3)
         Panel2.Name = "Panel2"
-        Panel2.Size = New Size(202, 52)
+        Panel2.Size = New Size(202, 34)
         Panel2.TabIndex = 2
         ' 
         ' Button1
         ' 
         Button1.BackColor = Color.Black
+        Button1.FlatAppearance.BorderSize = 0
+        Button1.FlatStyle = FlatStyle.Flat
         Button1.ForeColor = Color.White
         Button1.Image = CType(resources.GetObject("Button1.Image"), Image)
-        Button1.Location = New Point(-94, -6)
+        Button1.Location = New Point(-98, -3)
         Button1.Name = "Button1"
         Button1.Padding = New Padding(25, 0, 0, 0)
-        Button1.Size = New Size(272, 75)
+        Button1.Size = New Size(272, 58)
         Button1.TabIndex = 3
         Button1.Text = "                    Profil"
         Button1.UseVisualStyleBackColor = False
@@ -112,7 +121,7 @@ Partial Class Form1
         ' 
         Panel3.BackColor = Color.Black
         Panel3.Controls.Add(Button2)
-        Panel3.Location = New Point(213, 136)
+        Panel3.Location = New Point(219, 331)
         Panel3.Name = "Panel3"
         Panel3.Size = New Size(202, 52)
         Panel3.TabIndex = 4
@@ -133,8 +142,7 @@ Partial Class Form1
         ' Panel4
         ' 
         Panel4.BackColor = Color.Black
-        Panel4.Controls.Add(Button3)
-        Panel4.Location = New Point(213, 78)
+        Panel4.Location = New Point(473, 113)
         Panel4.Name = "Panel4"
         Panel4.Size = New Size(202, 52)
         Panel4.TabIndex = 5
@@ -142,21 +150,22 @@ Partial Class Form1
         ' Button3
         ' 
         Button3.BackColor = Color.Black
+        Button3.FlatAppearance.BorderSize = 0
+        Button3.FlatStyle = FlatStyle.Flat
         Button3.ForeColor = Color.White
         Button3.Image = CType(resources.GetObject("Button3.Image"), Image)
-        Button3.Location = New Point(-42, -13)
+        Button3.Location = New Point(-94, 39)
         Button3.Name = "Button3"
         Button3.Padding = New Padding(25, 0, 0, 0)
         Button3.Size = New Size(272, 75)
         Button3.TabIndex = 3
-        Button3.Text = "                    Cari Kost"
+        Button3.Text = "                       Cari Kost"
         Button3.UseVisualStyleBackColor = False
         ' 
         ' Panel5
         ' 
         Panel5.BackColor = Color.Black
-        Panel5.Controls.Add(Button4)
-        Panel5.Location = New Point(213, 194)
+        Panel5.Location = New Point(473, 171)
         Panel5.Name = "Panel5"
         Panel5.Size = New Size(202, 52)
         Panel5.TabIndex = 5
@@ -164,9 +173,11 @@ Partial Class Form1
         ' Button4
         ' 
         Button4.BackColor = Color.Black
+        Button4.FlatAppearance.BorderSize = 0
+        Button4.FlatStyle = FlatStyle.Flat
         Button4.ForeColor = Color.White
         Button4.Image = CType(resources.GetObject("Button4.Image"), Image)
-        Button4.Location = New Point(-46, -10)
+        Button4.Location = New Point(-94, 88)
         Button4.Name = "Button4"
         Button4.Padding = New Padding(25, 0, 0, 0)
         Button4.Size = New Size(272, 75)
@@ -178,7 +189,7 @@ Partial Class Form1
         ' 
         Panel6.BackColor = Color.Black
         Panel6.Controls.Add(Button5)
-        Panel6.Location = New Point(213, 252)
+        Panel6.Location = New Point(219, 389)
         Panel6.Name = "Panel6"
         Panel6.Size = New Size(202, 52)
         Panel6.TabIndex = 6
@@ -196,11 +207,84 @@ Partial Class Form1
         Button5.Text = "                    Profil"
         Button5.UseVisualStyleBackColor = False
         ' 
+        ' Panel7
+        ' 
+        Panel7.BackColor = Color.Black
+        Panel7.Controls.Add(Button4)
+        Panel7.Controls.Add(Button3)
+        Panel7.Controls.Add(Button6)
+        Panel7.Location = New Point(0, 84)
+        Panel7.Name = "Panel7"
+        Panel7.Size = New Size(167, 166)
+        Panel7.TabIndex = 7
+        ' 
+        ' Button6
+        ' 
+        Button6.BackColor = Color.Black
+        Button6.FlatAppearance.BorderSize = 0
+        Button6.FlatStyle = FlatStyle.Flat
+        Button6.ForeColor = Color.White
+        Button6.Image = CType(resources.GetObject("Button6.Image"), Image)
+        Button6.Location = New Point(-94, -6)
+        Button6.Name = "Button6"
+        Button6.Padding = New Padding(25, 0, 0, 0)
+        Button6.Size = New Size(272, 75)
+        Button6.TabIndex = 3
+        Button6.Text = "                    Menu"
+        Button6.UseVisualStyleBackColor = False
+        ' 
+        ' Panel8
+        ' 
+        Panel8.BackColor = Color.Black
+        Panel8.Controls.Add(Button7)
+        Panel8.Location = New Point(473, 229)
+        Panel8.Name = "Panel8"
+        Panel8.Size = New Size(202, 52)
+        Panel8.TabIndex = 7
+        ' 
+        ' Button7
+        ' 
+        Button7.BackColor = Color.Black
+        Button7.ForeColor = Color.White
+        Button7.Image = CType(resources.GetObject("Button7.Image"), Image)
+        Button7.Location = New Point(-60, -13)
+        Button7.Name = "Button7"
+        Button7.Padding = New Padding(25, 0, 0, 0)
+        Button7.Size = New Size(272, 75)
+        Button7.TabIndex = 3
+        Button7.Text = "                            Tentang Kami"
+        Button7.UseVisualStyleBackColor = False
+        ' 
+        ' Panel9
+        ' 
+        Panel9.BackColor = Color.Black
+        Panel9.Controls.Add(Button8)
+        Panel9.Location = New Point(473, 287)
+        Panel9.Name = "Panel9"
+        Panel9.Size = New Size(202, 52)
+        Panel9.TabIndex = 7
+        ' 
+        ' Button8
+        ' 
+        Button8.BackColor = Color.Black
+        Button8.ForeColor = Color.White
+        Button8.Image = CType(resources.GetObject("Button8.Image"), Image)
+        Button8.Location = New Point(-60, -13)
+        Button8.Name = "Button8"
+        Button8.Padding = New Padding(25, 0, 0, 0)
+        Button8.Size = New Size(272, 75)
+        Button8.TabIndex = 3
+        Button8.Text = "                    Keluar"
+        Button8.UseVisualStyleBackColor = False
+        ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(800, 450)
+        Controls.Add(Panel9)
+        Controls.Add(Panel7)
+        Controls.Add(Panel8)
         Controls.Add(Panel6)
         Controls.Add(Panel4)
         Controls.Add(Panel3)
@@ -215,9 +299,10 @@ Partial Class Form1
         FlowLayoutPanel1.ResumeLayout(False)
         Panel2.ResumeLayout(False)
         Panel3.ResumeLayout(False)
-        Panel4.ResumeLayout(False)
-        Panel5.ResumeLayout(False)
         Panel6.ResumeLayout(False)
+        Panel7.ResumeLayout(False)
+        Panel8.ResumeLayout(False)
+        Panel9.ResumeLayout(False)
         ResumeLayout(False)
     End Sub
 
@@ -235,5 +320,11 @@ Partial Class Form1
     Friend WithEvents Button4 As Button
     Friend WithEvents Panel6 As Panel
     Friend WithEvents Button5 As Button
+    Friend WithEvents Panel7 As Panel
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Panel8 As Panel
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Panel9 As Panel
+    Friend WithEvents Button8 As Button
 
 End Class
